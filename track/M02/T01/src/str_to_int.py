e22740b106a5d251787d44ba4e6f067de4a39caf
bharathicode <bharathirameshrk2@gmail.com>
2026-08-05 23:29:27 +0530

    type casting

---
+++ track/M02/T01/src/str_to_int.py
@@ -0,0 +1,13 @@
+age_text = "21"
+course_fee = "499.50"
+attempts = 3
+
+age = int(age_text)
+print(age)
+print(type(age))
+course = float(course_fee)
+print(course)
+print(type(course))
+attempt = str(attempts)
+print(attempt)
+print(type(attempt))
