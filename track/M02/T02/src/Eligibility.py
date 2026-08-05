17b33205a1d570cdd8a67bb7b42f7cb8227d593c
bharathicode <bharathirameshrk2@gmail.com>
2026-08-05 17:30:47 +0530

    Nested if else

---
+++ track/M02/T02/src/Eligibility.py
@@ -0,0 +1,11 @@
+marks = int(input())
+attendance = int(input())
+project_status = input()
+
+if(marks>=60 and attendance >=75):
+    if(project_status == "yes"):
+        print("Eligible")
+    else:
+        print("Not Eligible")
+else:
+    print("Not Eligible")
